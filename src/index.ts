3228e50b3132170d1fa40a490d26d3c1d41778eb
Jaemin0299 <78199903+Jaemin0299@users.noreply.github.com>
2021-10-24 15:15:04 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { events } from 'bdsx/event';
 import { latitude, longitude } from '../config.json';
 
 events.serverOpen.on(() => {
-    if (!latitude) return console.log(new Error(`latitude is not set! (Please correct "plugins\\real-time\\config.json"!)`));
-    if (!longitude) return console.log(new Error(`longitude is not set! (Please correct "plugins\\real-time\\config.json"!)`));
+    if (!latitude) return console.log(new Error(`latitude is not set!`));
+    if (!longitude) return console.log(new Error(`longitude is not set!`));
     import(`./setTime`)
 });
